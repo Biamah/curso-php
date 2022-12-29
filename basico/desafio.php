f19ce5dbc2aee43a50b8edcf131db1602a6cf6c3
Biamah <beatriz.souza.costa@outlook.com>
2022-12-29 17:31:36 -0300

    feat: basic challenge

---
+++ basico/desafio.php
@@ -0,0 +1,11 @@
+<div class="titulo">Desafio módulo básico</div>
+
+<?php 
+$calc = 4 + 4;
+?>
+
+<ul>
+    <li>2 + 2 = <?php echo 2 + 2; ?></li>
+    <li>4 + 4 = <?= $calc ?></li>
+    <li>8 + 8 = <? echo 8 + 8; ?></li>
+</ul>
--- index.php
+++ index.php
@@ -24,6 +24,7 @@
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
                         <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
                         <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=desafio">Desafio módulo básico</a></li>
                     </ul>
                 </div>
             </nav>
