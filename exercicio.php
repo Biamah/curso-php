7c04df4ef7c3c0461d431fd90d0cfa183ecfd35a
Biamah <beatriz.souza.costa@outlook.com>
2022-12-29 16:08:13 -0300

    feat: finalizes the template that will be used for the exercises

--- exercicio.php
+++ exercicio.php
@@ -4,18 +4,27 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
     <title>Exercício</title>
 </head>
-<body>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
 
         <h2>Visualização do exercício</h2>
     </header>
 
+    <nav class="navegacao">
+        <a href=<?= "{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+
     <main class="principal">
         <div class="conteudo">
-            
+            <?php
+                // include($_GET['dir'] . '/' . $_GET['file'] . '.php');
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
         </div>
     </main>
 
